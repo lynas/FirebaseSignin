37fae0372862e9284ed45179e36f9683f81ed0dc
LynAs <szlynas@gmail.com>
2017-05-06 00:10:39 +0600

    query possible in on pause

--- app/src/main/java/com/lynas/myapplication/BaseActivity.kt
+++ app/src/main/java/com/lynas/myapplication/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.lynas.myapplication
 
 import android.support.v7.app.AppCompatActivity
+import io.realm.Realm
+import java.util.*
 
 /**
  * Created by lynas on 5/3/2017.
@@ -10,5 +12,18 @@ open class BaseActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         println("On pause")
+
+        Realm.init(applicationContext)
+        val realm = Realm.getDefaultInstance()
+        realm.beginTransaction()
+        val person = realm.createObject(Person::class.java, UUID.randomUUID().toString())
+        person.name = "Sazzad"
+        val listOfPerson = realm.where(Person::class.java)
+        println("#############################################")
+        println(listOfPerson.count())
+
+
+        realm.commitTransaction()
+        realm.close()
     }
 }
