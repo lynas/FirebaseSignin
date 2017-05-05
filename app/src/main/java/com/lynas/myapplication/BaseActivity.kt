d69824be64bbfbaaaddd64b1d6d5877113c2e8aa
LynAs <szlynas@gmail.com>
2017-05-06 00:33:56 +0600

    update

--- app/src/main/java/com/lynas/myapplication/BaseActivity.kt
+++ app/src/main/java/com/lynas/myapplication/BaseActivity.kt
@@ -2,7 +2,6 @@ package com.lynas.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import io.realm.Realm
-import java.util.*
 
 /**
  * Created by lynas on 5/3/2017.
@@ -16,8 +15,6 @@ open class BaseActivity : AppCompatActivity() {
         Realm.init(applicationContext)
         val realm = Realm.getDefaultInstance()
         realm.beginTransaction()
-        val person = realm.createObject(Person::class.java, UUID.randomUUID().toString())
-        person.name = "Sazzad"
         val listOfPerson = realm.where(Person::class.java).findAll()
         println("#############################################")
         println(listOfPerson.count())
